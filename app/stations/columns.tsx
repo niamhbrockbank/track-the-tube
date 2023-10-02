91c69c41713b6a26e01bc60e9b1b15aa87148749
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-02 15:57:06 +0100

    ✨ Update column accessors

--- app/stations/columns.tsx
+++ app/stations/columns.tsx
@@ -2,10 +2,13 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import {Station } from "@/types/globals.types"
-import stations from "@/stations.json"
 
 /**
  * Define the core of what the table will look like
  * Define how the data will be displayed, formatted, sorted and filtered
  */
-export const columns: ColumnDef<Station>[] = stations
+export const columns: ColumnDef<Station>[] = [
+    {accessorKey : "name", header : "Station"},
+    {accessorKey: 'status', header:"Status"}, 
+    {accessorKey : "notes", header : "Notes"}
+]
