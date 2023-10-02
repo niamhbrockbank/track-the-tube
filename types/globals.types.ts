59d214d7122b7ae990d9782df894d39173ab5e08
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-02 15:11:57 +0100

    🏷️ Add station types

--- types/globals.types.ts
+++ types/globals.types.ts
@@ -1,17 +1,19 @@
 type BasicStation = {
-    id : string,
-    name: string,
-    lines : string[],
-    notes? : string
-}
+  id: string;
+  name: string;
+  lines: string[];
+  notes?: string;
+};
 
-type StationInteraction = {
-    status : 'none',
-} | {
-    status : 'visited' | 'passed through' | 'changed',
-    rating : 1 | 2 | 3 | 4 | 5,
-    dateOfFirstVisit : Date,
-    purposeOfVisit : string,
-}
+type StationInteraction =
+  | {
+      status: "none";
+    }
+  | {
+      status: "visited" | "passed through" | "changed";
+      rating: 1 | 2 | 3 | 4 | 5;
+      dateOfFirstVisit: Date;
+      purposeOfVisit: string;
+    };
 
-export type Station = BasicStation & StationInteraction
+export type Station = BasicStation & StationInteraction;
