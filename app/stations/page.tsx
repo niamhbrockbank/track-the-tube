f5614278260a94a7f60e049a096a4b86a0c33c07
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-02 15:51:08 +0100

    ✨ 💄 Add table with no data formatted

--- app/stations/page.tsx
+++ app/stations/page.tsx
@@ -1,1 +1,26 @@
-// Where the table is rendered
+import {columns } from "./columns"
+import {Station } from "@/types/globals.types"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Station[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "three",
+      name: "Victoria",
+      lines : ['Victoria'],
+      status : 'none'
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
