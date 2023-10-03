d500ed4dbc33eb69c0f0d9dc724329821efe5638
Niamh Brockbank <niamh.brockbank@national-ice-centre.com>
2023-10-03 17:26:56 +0100

    👽️ Fetch stations from API

--- app/stations/columns.tsx
+++ app/stations/columns.tsx
@@ -1,25 +1,39 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
-import {Station } from "@/types/globals.types"
+import { ColumnDef } from "@tanstack/react-table";
+import { Station } from "@/types/globals.types";
+import { Badge } from "@/components/ui/badge";
 
 /**
  * Define the core of what the table will look like
  * Define how the data will be displayed, formatted, sorted and filtered
  */
 export const columns: ColumnDef<Station>[] = [
-    {accessorKey : "name", header : "Station"},
-    {accessorKey: 'status', header:() => <div className="text-center">Status</div>, 
-    cell: ({row}) =>  <div className="text-center font-medium">{row.getValue('status')}</div>}
-    , 
-    {accessorKey : "notes", header : "Notes", cell: ({row}) => {
-        const note : string = row.getValue("notes")
-        if (!note) return
+  { accessorKey: "name", header: "Station" },
+  {
+    accessorKey: "status",
+    header: () => <div className="text-left">Status</div>,
+    cell: ({ row }) => (
+      <Badge variant="outline">{row.getValue("status")}</Badge>
+    ),
+  },
+  {
+    accessorKey: "notes",
+    header: "Notes",
+    cell: ({ row }) => {
+      const note: string = row.getValue("notes");
+      if (!note) return;
 
-        if (note.length > 10){
-            return <div className="text-right font-medium">{`${note.slice(0,9)}...`}</div>
-        }
+      if (note.length > 10) {
+        return (
+          <div className="text-right font-medium">{`${note.slice(
+            0,
+            9
+          )}...`}</div>
+        );
+      }
 
-        return <div className="text-right font-medium">{note}</div>
-    }}
-]
+      return <div className="text-right font-medium">{note}</div>;
+    },
+  },
+];
