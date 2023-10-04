1568ba05313e920c5affd563472ba213725cbb3a
Niamh Brockbank <niamh.brockbank@national-ice-centre.com>
2023-10-04 16:07:56 +0100

    💡 Remove outdated comments

--- app/stations/page.tsx
+++ app/stations/page.tsx
@@ -5,32 +5,6 @@ import stations from "@/stations.json";
 // import fs from "fs";
 
 async function getData(): Promise<Station[]> {
-  // Fetch data from your API here.
-
-  // let stations: Station[] = [];
-
-  // await fs.readFile("./stationList.csv", "utf8", (err, data) => {
-  //   if (err) throw err;
-
-  //   const rawStationNames = data.split(",");
-  //   const stationNames = rawStationNames.filter((n) => n !== "\r\n");
-
-  //   stations = stationNames.map((n) => {
-  //     return {
-  //       id: crypto.randomUUID(),
-  //       name: n.slice(0, -2),
-  //       lines: [],
-  //       status: "none",
-  //     };
-  //   });
-
-  //   fs.writeFile("./stations.json", JSON.stringify(stations), "utf8", (err) => {
-  //     if (err) throw err;
-  //     console.log("file saved");
-  //   });
-
-  //   console.log(stations);
-  // });
   const data: Station[] = stations;
   return data;
 }
