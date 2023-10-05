e1cd71d20539cd604c9125dd5e5f58733f763555
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-05 13:02:30 +0100

    🗃️ Create database schema

---
+++ db/schema.sql
@@ -0,0 +1,44 @@
+DROP TABLE stations IF EXISTS;
+DROP TABLE lines IF EXISTS;
+DROP TABLE stations_on_lines IF EXISTS;
+DROP TABLE user_data IF EXISTS;
+
+CREATE TABLE stations (
+    station_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE lines (
+    line_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    colour VARCHAR(50)
+);
+
+CREATE TABLE stations_on_lines (
+    line_id VARCHAR(255) REFERENCES lines(line_id),
+    station_id VARCHAR(255) REFERENCES stations(station_id),
+    PRIMARY KEY (line_id, station_id)
+);
+
+CREATE TYPE status AS ENUM ('visited', 'passed through', 'changed', 'none');
+
+CREATE TABLE user_data (
+    user_id SERIAL PRIMARY KEY,
+    station_id VARCHAR(255) REFERENCES stations(station_id),
+    user_name VARCHAR(255),
+    status status,
+    rating INT,
+    date_of_visit TIMESTAMP,
+    purpose_of_visit VARCHAR(255),
+
+    CONSTRAINT rating CHECK (rating >= 1 AND rating <= 5)
+);
+
+
+
+
+
+
+
+
+
