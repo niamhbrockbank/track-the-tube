ce8e6c7556beaeb8b75ee4083008344b8c00a28a
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-05 14:48:31 +0100

    🔐 Update secrets

--- lib/api/getStations.ts
+++ lib/api/getStations.ts
@@ -1,3 +1,17 @@
+import {Client} from "pg";
+import { config } from "dotenv";
+
 export default async function getStations(){
+    // Connect to database
+    config()
+
+    const dbConfig = {
+        connectionString : process.env.DATABASE_URL,
+        ssl: false
+    }
+
+    const client = new Client(dbConfig)
+    // Query database for stations
+    // Return stations
     return 'hi'
 }
