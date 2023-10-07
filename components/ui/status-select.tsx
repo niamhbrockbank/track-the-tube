b22d159195a3d1c471669cdb8d50e78efa3f4743
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-07 16:01:41 +0100

    🚧 Add prelim state handling

--- components/ui/status-select.tsx
+++ components/ui/status-select.tsx
@@ -6,25 +6,28 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Badge } from "./badge";
+import { useState } from "react";
 
+type Status = "visited" | "passed through" | "changed" | "none";
 interface IProps {
-  status: "visited" | "passed through" | "changed" | "none";
+  status: Status;
 }
 
 export default function StatusSelect({ status }: IProps) {
+  const [newStatus, setNewStatus] = useState<Status>("none");
   const variant = status === "passed through" ? "passedThrough" : status;
   const statusOptions = ["visited", "passed through", "changed", "none"].filter(
     (o) => o !== status
   );
 
   return (
-    <Select>
+    <Select onValueChange={(value: Status) => setNewStatus(value)}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder={<Badge variant={variant}>{status}</Badge>} />
       </SelectTrigger>
       <SelectContent>
-        {statusOptions.map((s) => (
-          <SelectItem value={s}>
+        {statusOptions.map((s, i) => (
+          <SelectItem value={s} key={i}>
             {/* @ts-ignore */}
             <Badge variant={s === "passed through" ? "passedThrough" : s}>
               {s}
