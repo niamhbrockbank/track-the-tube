d6de6f64011031583d891564ecf7d055038e6c10
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-09 11:33:48 +0100

    🔥 Remove unused import

--- app/api/lines/route.ts
+++ app/api/lines/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { db, connectDatabase } from "@/lib/db/db";
+import { db } from "@/lib/db/db";
 
 export async function GET() {
   try {
