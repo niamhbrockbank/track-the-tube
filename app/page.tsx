15578215a7dd06c5c765e0e45be504507525aeba
niamhbrockbank <niamh.brockbank@gmail.com>
2023-10-10 13:05:09 +0100

    ✨ Make use of home page button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me! </Button>
+      <Button>
+        <Link href="/stations">Log In</Link>
+      </Button>
     </main>
-  )
+  );
 }
