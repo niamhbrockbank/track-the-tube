f2df701d708870debb00a94a1d74abfc794d7222
Niamh Brockbank <niamh.brockbank@national-ice-centre.com>
2023-10-27 12:15:25 +0100

    ✨ Add sign in with google

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,55 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import Link from "next/link";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { redirect } from "next/navigation";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChUjIchFLMJrNCteRhcJKdD83AJL44rgc",
+  authDomain: "track-the-tube.firebaseapp.com",
+  projectId: "track-the-tube",
+  storageBucket: "track-the-tube.appspot.com",
+  messagingSenderId: "530623776222",
+  appId: "1:530623776222:web:d39255175adfb0eed2894f",
+  measurementId: "G-GLCFS3DQ7R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
 export default function Home() {
+  const [user] = useAuthState(auth);
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      const response = await signInWithPopup(auth, provider);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        <Link href="/stations">Sign in</Link>
-      </Button>
-      <Button>Sign in with Google</Button>
-    </main>
+    <>
+      {user ? (
+        <>{redirect("/stations")}</>
+      ) : (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <Button>
+            <Link href="/stations">Sign in</Link>
+          </Button>
+          <Button onClick={handleSignInWithGoogle}>Sign in with Google</Button>
+        </main>
+      )}
+    </>
   );
 }
