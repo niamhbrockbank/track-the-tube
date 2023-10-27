634a5114ccca95422d20c01fc9c7578a897d7dcc
Niamh Brockbank <niamh.brockbank@national-ice-centre.com>
2023-10-27 16:08:37 +0100

    💄 Start styling auth form

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -21,6 +21,16 @@ export default function UserAuthForm({ auth }: Props) {
     <>
       <Input placeholder="name@example.com" />
       <Button disabled>Coming Soon</Button>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
       <Button onClick={handleSignInWithGoogle}>Sign in with Google</Button>
       <Link
         href="/stations"
