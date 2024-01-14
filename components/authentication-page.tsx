e03c50cdb13e1504611a46a4df5f6fc3edf062a5
niamhbrockbank <niamh.brockbank@gmail.com>
2024-01-14 16:00:24 +0000

    🔥 remove outdated comments

--- components/authentication-page.tsx
+++ components/authentication-page.tsx
@@ -12,15 +12,6 @@ export default function AuthenticationPage({ auth }: Props): JSX.Element {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        {/* <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link> */}
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-[url('/tube-unsplash.jpg')] bg-cover" />
           <div className="relative z-20 flex items-center text-lg font-medium">
