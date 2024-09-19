b5e658c93aa9bd0e0ec59eb41d019b9e9b63c812
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-09-19 15:22:03 +0100

    Add alert circle to alerts

--- app/achievements/page.tsx
+++ app/achievements/page.tsx
@@ -2,12 +2,14 @@
 
 import NavBar from "@/components/nav-bar";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 
 export default function Achievements(): JSX.Element {
   return (
     <>
       <NavBar />
       <Alert className="m-3.5 max-w-screen-sm">
+        <AlertCircle className="h-4 w-4" />
         <AlertTitle>Coming Soon</AlertTitle>
         <AlertDescription>
           Earn achievements for visiting stations.
--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/nav-bar";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 
 export default function Leaderboard(): JSX.Element {
   return (
     <>
       <NavBar />
       <Alert className="m-3.5 max-w-screen-sm">
+        <AlertCircle className="h-4 w-4" />
         <AlertTitle>Coming Soon</AlertTitle>
         <AlertDescription>
           See how you compare to your friends.
